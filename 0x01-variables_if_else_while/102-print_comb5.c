74fafef1e6fd3de88f8623feded9db95c6a6cf28
HillaArts <hassenyehillary@gmail.com>
2023-02-20 10:39:21 -0800

    12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	for (i = 0 ; i < 100 ; i++)
+	{
+		for (j = i ; j < 100 ; j++)
+			if (i != j)
+			{
+				putchar('0' + i / 10);
+				putchar('0' + i % 10);
+				putchar(',');
+				putchar(' ');
+				putchar('0' + j / 10);
+				putchar('0' + j % 10);
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	putchar('\n')
+	return (0);
+}

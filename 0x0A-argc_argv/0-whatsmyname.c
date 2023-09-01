a6969809db379e5cad33aa84aca32842ce72139a
HillaArts <hassenyehillary@gmail.com>
2023-09-01 14:01:49 -0700

    a program that prints the number of arguments passed into it

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,13 +8,13 @@
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
-{
-        int i;
+}
+	int i;
 
-        for (i = 0; i < argc ; i++)
+	for (i = 0; i < argc ; i++)
         {
-                printf("%s\n", argv[i]);
+		printf("%s\n", argv[i]);
         }
-        return (0);
+	return (0);
 }
 

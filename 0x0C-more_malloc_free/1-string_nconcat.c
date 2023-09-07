23fa3797d1877b91b7dfbd68ded04ff4cc34a7c2
HillaArts <hassenyehillary@gmail.com>
2023-09-07 02:01:49 -0700

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,43 +11,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int i = 0, j = 0, k = 0, l = 0;
-        char *str;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        while (s1[i])
-                i++;
+	while (s1[i])
+		i++;
 
-        while (s2[k])
-                k++;
+	while (s2[k])
+		k++;
 
-        if (n >= k)
-                l = i + k;
-        else
-                l = i + n;
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
 
-        str = malloc(sizeof(char) * l + 1);
-        if (str == NULL)
-                return (NULL);
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
 
-        k = 0;
-        while (j < l)
-        {
-                if (j <= i)
-                        str[j] = s1[j];
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
 
-                if (j >= i)
-                {
-                        str[j] = s2[k];
-                        k++;
-                }
-                j++;
-        }
-        str[j] = '\0';
-        return (str);
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
 

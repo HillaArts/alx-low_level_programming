730be00b9c9d9aba5cb3c160883c304b5f213602
HillaArts <hassenyehillary@gmail.com>
2023-09-13 02:27:41 -0700

    program that prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -17,20 +17,20 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-                return (1);
+		return (1);
 	}
 
 	num_bytes = atoi(argv[1]);
 
 	if (num_bytes < 0)
 	{
-                printf("Error\n");
-                return (2);
+		printf("Error\n");
+		return (2);
 	}
 
 	for (i = 0; i < num_bytes; i++)
 	{
-                printf("%02x ", *((unsigned char *)main + i));
+		printf("%02x ", *((unsigned char *)main + i));
 	}
 
 	printf("\n");

e1b9a8d996398e62f8e991183c98fedcd73e7cac
HillaArts <hassenyehillary@gmail.com>
2023-09-28 09:42:04 -0700

    readme

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+    if (b == NULL)
+        return 0;
+
+    unsigned int result = 0;
+
+    while (*b != '\0')
+    {
+        if (*b == '0')
+        {
+            result <<= 1;
+        }
+        else if (*b == '1')
+        {
+            result <<= 1;
+            result |= 1;
+        }
+        else
+        {
+            return 0;
+        }
+        b++;
+    }
+
+    return result;
+}
+

